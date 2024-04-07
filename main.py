5727d5c90fb5c025af868411d0e53573c61410d2
oboy21 <oboy11b@gmail.com>
2024-04-07 16:22:39 +0300

    Day 24: Exercises: Add lines

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ df = pd.read_csv("topics.csv")
 for index, row in df.iterrows():
     pdf.add_page()
 
+    #Set the header
+
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
              ln=1)
-    pdf.line(10, 21, 200, 21)
+    for y in range(20, 298, 10):
+        pdf.line(10, y,200, y)
 
 
     #Set the footer
@@ -23,7 +26,7 @@ for index, row in df.iterrows():
 
 
 
-    for i in range(row["Pages"]):
+    for i in range(row["Pages"] -1 ):
         pdf.add_page()
 
         #Set the footer
@@ -31,5 +34,8 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
